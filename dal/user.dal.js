bfc5287db5c2c661f5a6313f11bdc08fae72cff3
Ankur <as797007@gmail.com>
2022-01-04 14:39:43 +0530

    Forgot password updated

--- dal/user.dal.js
+++ dal/user.dal.js
@@ -20,27 +20,59 @@ const storeUser = async (userToStore) => {
 };
 
 const findUserByUsername = async (username) => {
+  try{
   const user = await User.findOne(username);
   return user;
+  }
+  catch(error) {
+    res.json({
+      message : "Error Found",
+      err : error
+    })
+  }
 };
 
 const updateUser = async (userData) => {
+  try {
   const user = await User.findByIdAndUpdate(
     userData._id,
     { $set: userData.toUpdate },
     { new: true }
   );
   return user;
+  }
+  catch (error){
+    res.json({
+      message : "error found",
+      err : error
+    })
+  }
 };
 
 const findAll = async () => {
+  try {
   const user = await User.find({});
   return user;
+  }
+  catch (error){
+    res.json({
+      message : "error found",
+      err : error
+    })
+  }
 };
 
 const deleteAll = async () => {
+  try {
   const user = await User.remove({});
   return user;
+  }
+  catch (error){
+    res.json({
+      message : "error found",
+      err : error
+    })
+  }
 };
 
 module.exports = {
