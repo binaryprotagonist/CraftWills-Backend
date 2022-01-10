cf6134ac5275b8bc79441cd41ed7b14174194a23
Ankur <as797007@gmail.com>
2022-01-10 17:39:05 +0530

    Image Bug Fixed

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,7 +4,7 @@ const storage = multer.diskStorage({
     cb(null, "public/uploads");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + file.originalname);
+    cb(null, file.originalname+file.mimetype);
   },
 });
 
